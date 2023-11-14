7a723baa72eae27a4465a0b6da4f77dbbd88fd48
vincent verbergt <vincent.verbergt@gmail.com>
2023-11-14 13:52:56 +0100

    Made the propertychangesupport fire on every change.

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -2,11 +2,32 @@ package database;
 
 import models.Model;
 
+import java.beans.PropertyChangeSupport;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
-public interface Database {
-    Optional<Model> getById(Long id);
-    Optional<Model> create(Model model);
-    Optional<Model> update(Model model);
-    void deleteById(Long id);
+public abstract class Database<T extends Model> {
+    private final PropertyChangeSupport changes = new PropertyChangeSupport(this);
+
+    public abstract Optional<T> getById(Long id);
+    public final Optional<T> create(T model) {
+        changes.firePropertyChange("Model Creation", null, model);
+        return createImplementation(model);
+    }
+
+    public final Optional<T> update(T model) {
+        changes.firePropertyChange("Model Modification", getById(model.getId()).get(), model);
+        return updateImplementation(model);
+    }
+
+    public final void deleteById(Long id) {
+        changes.firePropertyChange("Model Deletion", getById(id).get(), null);
+        deleteByIdImplementation(id);
+    }
+
+    protected abstract Optional<T> updateImplementation(T model);
+    protected abstract void deleteByIdImplementation(Long id);
+
+    protected abstract Optional<T> createImplementation(T Model);
 }
