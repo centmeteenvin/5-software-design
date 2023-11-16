414eef06c73305f97971a41c9822d842ee6212f9
J0sa3210 <joeri.winckelmans@outlook.be>
2023-11-16 10:08:39 +0100

    Added tests for addTicket and removeTicket

--- src/test/java/controllers/PersonControllerImplementationTest.java
+++ src/test/java/controllers/PersonControllerImplementationTest.java
@@ -33,7 +33,7 @@ public class PersonControllerImplementationTest {
         mockTicketController = mock(TicketController.class);
         mockTicketCategoryController = mock(TicketCategoryController.class);
 
-        controller = new PersonControllerConcrete(mockPersonDatabase,mockTicketDatabase);
+        controller = new PersonControllerConcrete(mockPersonDatabase, mockTicketDatabase);
         controller.setTicketController(mockTicketController);
     }
 
@@ -42,16 +42,60 @@ public class PersonControllerImplementationTest {
     }
 
     @Test
-    void create(){
+    void create() {
         doReturn(Optional.empty()).when(mockPersonDatabase).getById(any());
 
         Optional<Person> receivedPerson = controller.create("foo");
         assertTrue(receivedPerson.isEmpty());
 
-        Person person = new Person(1L,"foo1",0);
+        Person person = new Person(1L, "foo1", 0);
         doReturn(Optional.of(person)).when(mockPersonDatabase).create(any());
         receivedPerson = controller.create("foo1");
         assertTrue(receivedPerson.isPresent());
         assertEquals(receivedPerson.get(), person);
+    }
+
+    @Test
+    void addTicket() {
+        doReturn(Optional.empty()).when(mockPersonDatabase).getById(any());
+        doReturn(Optional.empty()).when(mockTicketDatabase).getById(any());
+
+        Person testPerson = new Person(1L, "foo", 0);
+        Ticket testTicket = new Ticket(2L, 100, 1L);
+        controller.addTicket(testPerson.getId(), 2L);
+
+        assertTrue(testPerson.getTicketsId().isEmpty());
+
+        doReturn(Optional.of(testPerson)).when(mockPersonDatabase).getById(any());
+        controller.addTicket(testPerson.getId(), 2L);
+
+        assertTrue(testPerson.getTicketsId().isEmpty());
+
+        doReturn(Optional.of(testTicket)).when(mockTicketDatabase).getById(any());
+        controller.addTicket(testPerson.getId(), 2L);
+
+        assertEquals(testPerson.getTicketsId().size(), 1);
+        assertEquals(testPerson.getTicketsId().get(0), 2L);
+    }
+
+    @Test
+    void removeTicket() {
+        doReturn(Optional.empty()).when(mockPersonDatabase).getById(any());
+        doReturn(Optional.empty()).when(mockPersonDatabase).update(any());
+
+        Person testPerson = new Person(1L, "foo", 0);
+        testPerson.getTicketsId().add(1L);
+
+        controller.removeTicket(testPerson.getId(), 1L);
+
+        assertEquals(testPerson.getTicketsId().get(0), 1L);
+        verify(mockPersonDatabase, never()).update(any());
+        
+        doReturn(Optional.of(testPerson)).when(mockPersonDatabase).getById(any());
+
+        controller.removeTicket(testPerson.getId(), 1L);
+
+        verify(mockPersonDatabase, times(1)).update(any());
+        assertTrue(testPerson.getTicketsId().isEmpty());
     }
 }
