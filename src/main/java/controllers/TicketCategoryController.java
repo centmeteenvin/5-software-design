3c1700eb60bc384d53402c43576c89b1f0dbd6e8
J0sa3210 <joeri.winckelmans@outlook.be>
2023-11-17 09:17:26 +0100

    Added all methods in TicketCategoryController

--- src/main/java/controllers/TicketCategoryController.java
+++ src/main/java/controllers/TicketCategoryController.java
@@ -4,9 +4,11 @@ import database.Database;
 import models.Ticket;
 import models.TicketCategory;
 
+import java.util.Optional;
+
 public abstract class TicketCategoryController {
-    private final Database<TicketCategory> ticketCategoryDatabase;
-    private final Database<Ticket> ticketDatabase;
+    protected final Database<TicketCategory> ticketCategoryDatabase;
+    protected final Database<Ticket> ticketDatabase;
 
     protected TicketCategoryController(Database<TicketCategory> ticketCategoryDatabase, Database<Ticket> ticketDatabase) {
         this.ticketCategoryDatabase = ticketCategoryDatabase;
@@ -16,7 +18,7 @@ public abstract class TicketCategoryController {
     /**
      * Creates a ticketCategory with the given name.
      */
-    public abstract TicketCategory create(String Name);
+    public abstract Optional<TicketCategory> create(String name);
 
     /**
      * adds a ticket to the current category.
--- src/main/java/controllers/TicketCategoryControllerImplementation.java
+++ src/main/java/controllers/TicketCategoryControllerImplementation.java
@@ -1,41 +1,82 @@
 package controllers;
 
 import database.Database;
+import models.Person;
 import models.Ticket;
 import models.TicketCategory;
 
-public class TicketCategoryControllerImplementation extends TicketCategoryController{
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+public class TicketCategoryControllerImplementation extends TicketCategoryController {
+    private TicketController ticketController;
+
     protected TicketCategoryControllerImplementation(Database<TicketCategory> ticketCategoryDatabase, Database<Ticket> ticketDatabase) {
         super(ticketCategoryDatabase, ticketDatabase);
     }
 
     @Override
-    public TicketCategory create(String Name) {
-        return null;
+    public void setTicketController(TicketController ticketController) {
+        this.ticketController = ticketController;
+    }
+
+    @Override
+    public Optional<TicketCategory> create(String name) {
+        long id = System.nanoTime();
+        Optional<TicketCategory> ticketCategory = ticketCategoryDatabase.create(new TicketCategory(id, name));
+
+        return ticketCategory;
     }
 
     @Override
     public void addTicket(Long id, Long ticketId) {
+        Optional<TicketCategory> category = ticketCategoryDatabase.getById(id);
+        if (category.isEmpty()) return;
 
+        Optional<Ticket> ticket = ticketDatabase.getById(ticketId);
+        if (ticket.isEmpty()) return;
+
+        category.get().getTicketIds().add(ticketId);
+        ticketCategoryDatabase.update(category.get());
     }
 
     @Override
     public void removeTicket(Long id, Long ticketId) {
+        Optional<TicketCategory> category = ticketCategoryDatabase.getById(id);
+        if (category.isEmpty()) return;
 
+        Optional<Ticket> ticket = ticketDatabase.getById(ticketId);
+        if (ticket.isEmpty()) return;
+
+        try {
+            category.get().getTicketIds().remove(ticketId);
+            ticketCategoryDatabase.update(category.get());
+        } catch (NullPointerException ignored) {
+        }
     }
 
     @Override
     public void rename(Long id, String newName) {
-
+        Optional<TicketCategory> category = ticketCategoryDatabase.getById(id);
+        if (category.isPresent()) {
+            category.get().setName(newName);
+            ticketCategoryDatabase.update(category.get());
+        }
     }
 
     @Override
     public void delete(Long id) {
-
-    }
-
-    @Override
-    public void setTicketController(TicketController ticketController) {
-
+        Optional<TicketCategory> category = ticketCategoryDatabase.getById(id);
+        if (category.isPresent()) {
+            Iterator<Long> it = category.get().getTicketIds().iterator();
+            while (it.hasNext()){
+                long ticketId = it.next();
+                ticketController.changeCategory(ticketId,0L);
+                it.remove();
+            }
+            ticketCategoryDatabase.deleteById(id);
+        }
     }
 }
