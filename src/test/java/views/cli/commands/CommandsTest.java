3887df9eac407a40778271eb4a4b211206d442b8
vincent verbergt <vincent.verbergt@gmail.com>
2023-11-18 22:22:15 +0100

    Implemented a test that ensures the correct constructors are present.

--- src/test/java/views/cli/commands/CommandsTest.java
+++ src/test/java/views/cli/commands/CommandsTest.java
@@ -1,6 +1,7 @@
 package views.cli.commands;
 
 import org.junit.jupiter.api.Test;
+import views.cli.ViewCommandLine;
 
 import java.util.Optional;
 
@@ -13,5 +14,13 @@ class CommandsTest {
         Optional<Command> result = Commands.parse(new String[]{CommandHelp.commandString}, null);
         assertTrue(result.isPresent());
         assertTrue(result.get() instanceof CommandHelp);
+    }
+
+    @Test
+    void testAllConstructorsPresent() {
+        for (Commands command : Commands.values()) {
+            assertDoesNotThrow(() -> command.commandClass.getConstructor(), "Forgot to implement empty constructor " + command.commandClass.getName());
+            assertDoesNotThrow(() -> command.commandClass.getConstructor(String[].class, ViewCommandLine.class), "Forgot to implement constructor with parameters in " + command.commandClass.getName());
+        }
     }
 }
