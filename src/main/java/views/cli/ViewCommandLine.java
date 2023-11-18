633f109c7fc229fc81f0ceca9d9dbf3f6f9b632d
vincent verbergt <vincent.verbergt@gmail.com>
2023-11-18 20:39:05 +0100

    Implemented the run loop in CommandLineView

--- src/main/java/views/cli/ViewCommandLine.java
+++ src/main/java/views/cli/ViewCommandLine.java
@@ -8,14 +8,20 @@ import models.Person;
 import models.Ticket;
 import models.TicketCategory;
 import views.View;
+import views.cli.commands.Command;
+import views.cli.commands.Commands;
 import views.cli.io.Input;
 import views.cli.io.InputStandard;
 import views.cli.io.Output;
 import views.cli.io.OutputStandard;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class ViewCommandLine extends View {
     public final Output output;
     public final Input input;
+
     public ViewCommandLine(
             Database<Person> personDatabase, Database<Ticket> ticketDatabase,
             Database<TicketCategory> ticketCategoryDatabase, PersonController personController,
@@ -31,9 +37,21 @@ public class ViewCommandLine extends View {
     public void run() {
         output.print("""
                 Welcome To The Script Kiddos' Money Tracker AKA: SKMT.
-                
+                                
                 To get started you can type "help".
                 This will show you all available commands.
+                                
+                To exit the program type: "exit".
                 """);
+
+        while (true) {
+            String inputString = input.input("");
+            if (Objects.equals(inputString, "exit")) break;
+            Optional<Command> command = Commands.parse(inputString.split(" "));
+            if (command.isEmpty()) output.print("Command Not Found\n");
+            else {
+                command.get().execute();
+            }
+        }
     }
 }
