a804ea19588f0923c720bec1c3e52a7254d3b650
vincent verbergt <vincent.verbergt@gmail.com>
2023-11-19 10:35:06 +0100

    Implemented execute function

--- src/main/java/views/cli/commands/CommandTicket.java
+++ src/main/java/views/cli/commands/CommandTicket.java
@@ -1,21 +1,43 @@
 package views.cli.commands;
 
+import views.cli.ViewCommandLine;
+
 public class CommandTicket extends Command{
     public static final String commandString = "ticket";
 
+    /**
+     * @param args the arguments that are given with the command. [0] equals {@link #commandString}.
+     * @param view
+     */
+    public CommandTicket(String[] args, ViewCommandLine view) {
+        super(args, view);
+    }
+
+    public CommandTicket() {
+    }
+
     @Override
     public void execute() {
-
+        assert view != null;
+        if (args.length == 1) {
+            view.output.print(incorrectNumberOfArguments(2, 1));
+            return;
+        }
+        switch (args[1]) {
+            default -> view.output.print("! Command not found, consider consulting {help ticket}\n");
+        }
     }
 
     @Override
     public String shortDescription() {
-        return null;
+        return "Main entrypoint for most ticket related commands";
     }
 
     @Override
     public String description() {
-        return null;
+        return """
+                % Main entrypoint for most ticket related commands
+                """;
     }
 
     @Override
--- src/test/java/views/cli/commands/CommandTicketTest.java
+++ src/test/java/views/cli/commands/CommandTicketTest.java
@@ -1,13 +1,34 @@
 package views.cli.commands;
 
 import org.junit.jupiter.api.Test;
+import views.cli.ViewCommandLine;
+import views.cli.io.Output;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 class CommandTicketTest extends CommandTest {
 
     @Test
     void execute() {
+        Output output = mock(Output.class);
+        ViewCommandLine view = new ViewCommandLine(null, null, null,
+                null, null, null,
+                null, output);
+        String[] args = new String[]{"ticket"};
+        CommandTicket commandTicket = new CommandTicket(args, view);
+        doNothing().when(output).print(anyString());
+
+        commandTicket.execute();
+
+        verify(output, times(1)).print(Command.incorrectNumberOfArguments(2, 1));
+
+        args = new String[]{"ticket", "foo"};
+        commandTicket = new CommandTicket(args, view);
+
+        commandTicket.execute();
+        verify(output, times(1)).print("! Command not found, consider consulting {help ticket}\n");
     }
 
     @Override
