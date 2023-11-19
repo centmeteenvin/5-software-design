956c85989d6396caac6b183ac8d2262e8c076732
vincent verbergt <vincent.verbergt@gmail.com>
2023-11-19 10:24:43 +0100

    Created empty CommandTicket class

---
+++ src/main/java/views/cli/commands/CommandTicket.java
@@ -0,0 +1,25 @@
+package views.cli.commands;
+
+public class CommandTicket extends Command{
+    public static final String commandString = "ticket";
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public String shortDescription() {
+        return null;
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+    @Override
+    public String getCommandString() {
+        return commandString;
+    }
+}
