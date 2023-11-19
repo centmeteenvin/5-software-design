934c487ddbbdb0946ac71e13b5cbc6e493796d44
vincent verbergt <vincent.verbergt@gmail.com>
2023-11-19 11:27:31 +0100

    Implemented execute method

--- src/main/java/views/cli/commands/CommandCategories.java
+++ src/main/java/views/cli/commands/CommandCategories.java
@@ -1,6 +1,9 @@
 package views.cli.commands;
 
+import models.TicketCategory;
 import views.cli.ViewCommandLine;
+
+import java.util.List;
 
 public class CommandCategories extends Command {
     public static final String commandString = "categories";
@@ -37,6 +40,14 @@ public class CommandCategories extends Command {
 
     @Override
     public void execute() {
-
+        assert view != null;
+        if (args.length != 1) {
+            view.output.print(incorrectNumberOfArguments(1, args.length));
+            return;
+        }
+        List<TicketCategory> categories = view.getTicketCategoryDatabase().getAll();
+        for (TicketCategory category : categories) {
+            view.output.print("%% %s -> %s\n".formatted(category.getId(), category.getName()));
+        }
     }
 }
--- src/test/java/views/cli/commands/CommandCategoriesTest.java
+++ src/test/java/views/cli/commands/CommandCategoriesTest.java
@@ -1,13 +1,47 @@
 package views.cli.commands;
 
+import database.Database;
+import models.TicketCategory;
 import org.junit.jupiter.api.Test;
+import views.cli.ViewCommandLine;
+import views.cli.io.Output;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 class CommandCategoriesTest extends CommandTest {
 
     @Test
     void execute() {
+        //noinspection unchecked
+        Database<TicketCategory> db = (Database<TicketCategory>) mock(Database.class);
+        Output output = mock(Output.class);
+        ViewCommandLine view = new ViewCommandLine(null, null, db,
+                null, null, null,
+                null, output);
+        String[] args = new String[]{CommandCategories.commandString, "foo"};
+        CommandCategories command = new CommandCategories(args, view);
+        doNothing().when(output).print(anyString());
+
+        command.execute();
+
+        verify(output, times(1)).print(Command.incorrectNumberOfArguments(1, 2));
+
+        args = new String[]{CommandCategories.commandString};
+        command = new CommandCategories(args, view);
+        List<TicketCategory> categories = List.of(
+          new TicketCategory(1L, "foo"),
+          new TicketCategory(2L, "bar")
+        );
+        doReturn(categories).when(db).getAll();
+
+        command.execute();
+
+        verify(output, times(1)).print("% 1 -> foo\n");
+        verify(output, times(1)).print("% 2 -> bar\n");
     }
 
     @Override
