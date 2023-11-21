a0f03d9f64886b03deefb0bc9c993da51b176782
J0sa3210 <joeri.winckelmans@outlook.be>
2023-11-21 17:33:13 +0100

    Made initial frame

--- src/main/java/views/gui/ViewJ2D.java
+++ src/main/java/views/gui/ViewJ2D.java
@@ -8,14 +8,24 @@ import models.Person;
 import models.Ticket;
 import models.TicketCategory;
 import views.View;
+import views.gui.components.ViewFrame;
+
+import javax.swing.*;
 
 public class ViewJ2D extends View {
+    JFrame frame = new ViewFrame();
+
+
     public ViewJ2D(Database<Person> personDatabase, Database<Ticket> ticketDatabase, Database<TicketCategory> ticketCategoryDatabase, PersonController personController, TicketController ticketController, TicketCategoryController ticketCategoryController) {
         super(personDatabase, ticketDatabase, ticketCategoryDatabase, personController, ticketController, ticketCategoryController);
     }
 
     @Override
     public void run() {
+        setupFrame();
+    }
 
+    public void setupFrame(){
+        frame.setVisible(true);
     }
 }
