d65e35ce4a1cf0600c2a50ab4be47d2c16670600
J0sa3210 <joeri.winckelmans@outlook.be>
2023-11-22 19:06:59 +0100

    Changed ViewFrame so Samplepanel is visible

--- src/main/java/views/gui/components/ViewFrame.java
+++ src/main/java/views/gui/components/ViewFrame.java
@@ -1,5 +1,7 @@
 package views.gui.components;
 
+import views.gui.components.panels.SamplePanel;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,6 +9,8 @@ public class ViewFrame extends JFrame {
     public ViewFrame() {
         this.setTitle("SKMT - Script Kiddos' Money Tracker");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setSize(screenSize);
+
+        this.setSize(new Dimension(screenSize.width-100, screenSize.height-50));
+        this.add(new SamplePanel(1));
     }
 }
