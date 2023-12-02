809db2ec113271f0d3d915325fc6af92c1f2d4bf
J0sa3210 <joeri.winckelmans@outlook.be>
2023-12-02 14:49:45 +0100

    Updated HomePanel

--- src/main/java/views/gui/components/panels/HomePanel.java
+++ src/main/java/views/gui/components/panels/HomePanel.java
@@ -1,42 +1,25 @@
 package views.gui.components.panels;
 
+import views.gui.styles.Style;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
 
 public class HomePanel extends JPanel {
     Border testBorder = BorderFactory.createLineBorder(Color.RED, 1);
 
     public HomePanel() {
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        this.setBackground(Color.BLUE);
+        this.setBackground(Style.getBackgroundColor_primary());
         this.add(createTitleSection());
         this.add(Box.createVerticalStrut(70));
         this.add(createButtonSection());
 
-
         //this.setBorder(testBorder);
-
-
-    }
-
-    private Font addFont(String fontName, float size) {
-        try {
-            Font font = Font.createFont(Font.TRUETYPE_FONT, new File("src/main/resources/fonts/" + fontName + ".ttf")).deriveFont(size);
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(font);
-            return font;
-        } catch (IOException | FontFormatException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     JPanel createTitleSection() {
-        Font titleFont = addFont("OpenSans-Bold", 122);
-        Font subtitleFont = addFont("OpenSans-Medium", 54);
-
 
         JPanel titleSection = new JPanel();
         titleSection.setLayout(new BoxLayout(titleSection, BoxLayout.Y_AXIS));
@@ -44,45 +27,51 @@ public class HomePanel extends JPanel {
 
         JLabel title = new JLabel("SKMT");
         title.setForeground(Color.WHITE);
-        title.setFont(titleFont);
+        title.setFont(Style.getTitleFont());
         title.setAlignmentX(CENTER_ALIGNMENT);
 
 
         JLabel subTitle = new JLabel("The Scripk Kiddo's Money Tracker");
         subTitle.setForeground(Color.WHITE);
-        subTitle.setFont(subtitleFont);
+        subTitle.setFont(Style.getSubtitleFont());
         subTitle.setAlignmentX(CENTER_ALIGNMENT);
 
         titleSection.add(title);
         titleSection.add(subTitle);
 
-        //titleSection.setBorder(testBorder);
+        titleSection.setBorder(testBorder);
         return titleSection;
     }
 
     JPanel createButtonSection() {
-        Font buttonFont = addFont("OpenSans-Bold", 30);
-
+        Font buttonFont = Style.getButtonFont();
+        Dimension buttonSize = new Dimension(450,150);
 
         JPanel buttonSection = new JPanel();
         buttonSection.setLayout(new BoxLayout(buttonSection, BoxLayout.X_AXIS));
         buttonSection.setBackground(null);
+        buttonSection.setSize(1500,500);
 
         JButton userButton = new JButton("Users");
-        userButton.setForeground(Color.BLUE);
+        userButton.setForeground(Style.getButtonForegroundColor());
         userButton.setFont(buttonFont);
-        userButton.setSize(300, 100);
-        userButton.setBackground(Color.WHITE);
+        userButton.setSize(buttonSize);
+        userButton.setPreferredSize(buttonSize);
+        userButton.setBackground(Style.getButtonBackgroundColor());
+
 
         JButton ticketButton = new JButton("Tickets");
-        ticketButton.setForeground(Color.BLUE);
+        ticketButton.setForeground(Style.getButtonForegroundColor());
         ticketButton.setFont(buttonFont);
+        ticketButton.setSize(buttonSize);
+        ticketButton.setPreferredSize(buttonSize);
+        ticketButton.setBackground(Style.getButtonBackgroundColor());
 
         buttonSection.add(userButton);
         buttonSection.add(Box.createHorizontalStrut(50));
         buttonSection.add(ticketButton);
 
-        //buttonSection.setBorder(testBorder);
+        buttonSection.setBorder(testBorder);
         return buttonSection;
     }
 }
--- src/main/java/views/gui/styles/Style.java
+++ src/main/java/views/gui/styles/Style.java
@@ -6,12 +6,27 @@ import java.io.IOException;
 
 public record Style() {
     // Colors
-    static Color backgroundColor_primary = Color.BLUE;
+    //Background
+    static Color backgroundColor_primary = new Color(0,45,179);
     static Color backgroundColor_secondary = Color.WHITE;
+    // Buttons
+    static Color buttonForegroundColor = new Color(0,45,179);
+    static Color buttonBackgroundColor = Color.WHITE;
 
     // Fonts
     static Font titleFont = addFont("OpenSans-Bold", 122);
     static Font subtitleFont = addFont("OpenSans-Medium", 54);
+    static Font buttonFont = addFont("OpenSans-Bold", 30);
+
+
+    // Getters
+    public static Color getButtonForegroundColor() {
+        return buttonForegroundColor;
+    }
+
+    public static Color getButtonBackgroundColor() {
+        return buttonBackgroundColor;
+    }
 
     public static Color getBackgroundColor_primary() {
         return backgroundColor_primary;
@@ -28,6 +43,11 @@ public record Style() {
     public static Font getSubtitleFont() {
         return subtitleFont;
     }
+
+    public static Font getButtonFont() {
+        return buttonFont;
+    }
+
 
     private static Font addFont(String fontName, float size) {
         try {
