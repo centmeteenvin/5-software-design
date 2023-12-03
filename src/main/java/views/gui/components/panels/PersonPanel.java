44ac745c299ab9f3e24b6a05cce9f53cf8cfc027
J0sa3210 <joeri.winckelmans@outlook.be>
2023-12-03 20:14:01 +0100

    Added a add user button

--- src/main/java/views/gui/components/panels/PersonPanel.java
+++ src/main/java/views/gui/components/panels/PersonPanel.java
@@ -1,6 +1,7 @@
 package views.gui.components.panels;
 
 import views.gui.styles.Style;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -17,17 +18,17 @@ public class PersonPanel extends JPanel {
         this.setLayout(new BorderLayout());
 
         this.add(createLeftPanel(), BorderLayout.LINE_START);
-        this.add(createRightPanel(),BorderLayout.CENTER);
+        this.add(createRightPanel(), BorderLayout.CENTER);
     }
 
-    JPanel createLeftPanel(){
+    JPanel createLeftPanel() {
         JPanel leftPanel = new JPanel();
-        leftPanel.setLayout(new BoxLayout(leftPanel,BoxLayout.Y_AXIS));
-
-        leftPanel.setPreferredSize(new Dimension(screenSize.width/4, screenSize.height));
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        leftPanel.setPreferredSize(new Dimension(screenSize.width / 4, screenSize.height));
         leftPanel.setBackground(style.getBackgroundColor_primary());
 
-        JLabel userLabel = new JLabel("Users"){
+        // Title (box 1)
+        JLabel userLabel = new JLabel("Users") {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
@@ -43,16 +44,32 @@ public class PersonPanel extends JPanel {
                 g.drawLine(0, height - 2, width, height - 2);
             }
         };
-        userLabel.setMaximumSize(new Dimension(screenSize.width/4,100));
+        userLabel.setMaximumSize(new Dimension(screenSize.width / 4, 100));
         userLabel.setForeground(Color.WHITE);
         userLabel.setFont(style.getBoldSubtitleFont());
         userLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
+        userLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         leftPanel.add(userLabel);
+
+        // Add buffer
+        leftPanel.add(Box.createVerticalStrut(30));
+
+        // Add Person button (Box2)
+        JButton createPersonButton = new JButton("+ Add User");
+        createPersonButton.setForeground(style.getButtonForegroundColor());
+        createPersonButton.setBackground(style.getButtonBackgroundColor());
+        createPersonButton.setFont(style.getButtonFont());
+        createPersonButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        createPersonButton.addActionListener(e -> {
+            System.out.println("Add user");
+        });
+        leftPanel.add(createPersonButton);
+
+
         return leftPanel;
     }
 
-    JPanel createRightPanel(){
+    JPanel createRightPanel() {
         JPanel rightPanel = new JPanel();
         rightPanel.setBackground(style.getBackgroundColor_secondary());
         return rightPanel;
