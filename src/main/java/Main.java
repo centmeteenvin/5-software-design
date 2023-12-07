c5ab486f1c9cf78d949ab263fc0194cf7e02a968
vincent verbergt <vincent.verbergt@gmail.com>
2023-12-07 10:04:41 +0100

    Added program arguments to program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,17 @@ import factories.ApplicationFactoryProductionGUI;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationFactory factory = new ApplicationFactoryProductionGUI();
+
+        ApplicationFactory factory;
+        if (args.length == 0) {
+            factory = new ApplicationFactoryProductionGUI();
+        } else {
+            switch (args[0]) {
+                case "cli" -> factory = new ApplicationFactoryProductionCLI();
+                case "gui" -> factory = new ApplicationFactoryProductionGUI();
+                default -> throw new IllegalArgumentException("Unexpected arguments where given, valid options are: [cli, gui]");
+            }
+        }
         Application application = new Application(factory);
         application.run();
     }
